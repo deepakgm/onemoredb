82bd8bb4deea50c9fdbe0bd51a5b86d8b2bdaf4a
deepakgm <dgopal31@gmail.com>
2020-02-03 10:56:31 -0500

    updated gtest

--- gtest.cc
+++ gtest.cc
@@ -12,23 +12,22 @@ using ::testing::Invoke;
 using ::testing::Return;
 using ::testing::_;
 
-const char *dbfile_dir = "/home/gurpreet/Desktop/dbi/onemoredb/test";
-const char *tpch_dir ="/home/gurpreet/Desktop/test/"; // dir where dbgen tpch files (extension *.tbl) can be found
-const char *catalog_path = "/home/gurpreet/Desktop/dbi/onemoredb/catalog"; // full path of the catalog file
+using namespace std;
 
-//class MockDBFile : public DBFile
-//{
-//public:
-//    MOCK_METHOD0(void,MoveFirst, ());
-//
-//};
+char dbfile_dir[PATH_MAX];
 
 int main(int argc, char **argv) {
+    if (getcwd(dbfile_dir, sizeof(dbfile_dir)) != NULL) {
+        clog <<"current working dir:" << dbfile_dir << endl;
+        strcat(dbfile_dir,"/test/test.bin");
+    } else {
+        cerr << "error while getting curent dir" << endl;
+        return 1;
+    }
+
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-
-
 
 
 class DBFileTest : public ::testing::Test {
@@ -38,39 +37,41 @@ protected:
     virtual void SetUp() {
         clog << "creating DBFile instance.." << endl;
         dbFile=new DBFile();
+        remove(dbfile_dir);
     }
 
     virtual void TearDown() {
         clog << "clearing memory.." << endl;
         delete dbFile;
+        remove(dbfile_dir);
     }
 };
 
-TEST_F(DBFileTest,openTest1){
-    //given
-    int res=0;
-//    when
-//    clog << "here" << f_path<< endl;
-    res=dbFile->Open("/home/gurpreet/Desktop/temp/t");
-    //assert
-    ASSERT_EQ(res,0);
-}
-
-TEST_F(DBFileTest,openTest2){
-    //given
-    int res=0;
-//    when
-    res=dbFile->Open(nullptr);
-    //assert
-    ASSERT_EQ(res,0);
-}
+//TEST_F(DBFileTest,openTest1){
+//    //given
+//    int res=0;
+////    when
+////    clog << "here" << f_path<< endl;
+//    res=dbFile->Open("/home/gurpreet/Desktop/temp/t");
+//    //assert
+//    ASSERT_EQ(res,0);
+//}
+//
+//TEST_F(DBFileTest,openTest2){
+//    //given
+//    int res=0;
+////    when
+//    res=dbFile->Open(nullptr);
+//    //assert
+//    ASSERT_EQ(res,0);
+//}
 
 TEST_F(DBFileTest,createTest1){
     //given
     int res=0;
 //    when
 //    clog << "here" << f_path<< endl;
-    res=dbFile->Create("/home/gurpreet/Desktop/temp/t",heap, nullptr);
+    res=dbFile->Create(dbfile_dir,heap, nullptr);
     //assert
     ASSERT_EQ(res,0);
 }
@@ -80,18 +81,10 @@ TEST_F(DBFileTest,createTest2){
     int res=0;
 //    when
 //    clog << "here" << f_path<< endl;
-    res=dbFile->Create("/home/gurpreet/Desktop/temp/t",sorted, nullptr);
+    res=dbFile->Create(dbfile_dir,sorted, nullptr);
     //assert
     ASSERT_EQ(res,1);
 }
 
-//TEST_F(DBFileTest,openTest3){
-//    MockDBFile mockDbFile;
-//
-////    EXPECT_CALL(mockDbFile,MoveFirst() )
-////            .Times(1);
-////    mockDbFile.MoveFirst();
-//
-//}
 
 
